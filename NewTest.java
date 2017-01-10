9b19f311c1cfdb6f386091d7d962aaeefd522873
Dushyant_Joshi <Dushyant_Joshi@IBM629-R9TAY2M>
2017-01-10 15:44:45 +0530

    test code changes

---
+++ NewTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.gecko.driver","C:/Users/IBM_ADMIN/Desktop/TADDM/Softwares/geckodriver-v0.11.1-win64/geckodriver.exe");
+	  WebDriver wd = new FireFoxDriver();
+	  wd.get("http://9.17.237.67:9080/SCOPEWEB");
+	  System.out.println("Adding comment");
+	  
+  }
+  
+}
