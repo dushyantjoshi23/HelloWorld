1ab1db101f5d5769ba29c76e118a7f9276e19d88
dushyantjoshi23 <dushyantjoshi23@gmail.com>
2017-01-10 15:59:56 +0530

    Update NewTest.java

--- NewTest.java
+++ NewTest.java
@@ -7,7 +7,7 @@ public class NewTest {
 	  System.setProperty("webdriver.gecko.driver","C:/Users/IBM_ADMIN/Desktop/TADDM/Softwares/geckodriver-v0.11.1-win64/geckodriver.exe");
 	  WebDriver wd = new FireFoxDriver();
 	  wd.get("http://9.17.237.67:9080/SCOPEWEB");
-	  System.out.println("ddAdding comt");
+	  System.out.println("mmAdding comt");
 	  System.out.println("ddAdding comt");
 	  
   }
